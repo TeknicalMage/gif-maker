46cb315d84262aece6495f86ca96bbc1d26d4e61
TeknicalMage <arcazjordan@gmail.com>
2021-10-22 05:16:09 -0400

    fsd
    sdfsdf

--- gif_creator_transparency.sh
+++ gif_creator_transparency.sh
@@ -2,3 +2,4 @@
 
 ffmpeg   -framerate 60   -pattern_type glob   -i '*.png'   -r 15   -lavfi split[v],palettegen,[v]paletteuse out.gif ;
 
+#A bit ghetto, but run this in the same direcotry as your set of png files you'd like ot convert
